dd87ce8dd7843ac2ffafe892cb7e581348b6c4c2
Simon Parten <quafadas@gmail.com>
2024-08-15 07:54:23 +0200

    tone js

--- hello.scala
+++ hello.scala
@@ -2,10 +2,7 @@
 //> using platform js
 
 //> using dep org.scala-js::scalajs-dom::2.8.0
-//> using dep com.raquo::laminar::17.0.0
-//> using dep com.raquo::waypoint::8.0.0
-//> using dep com.lihaoyi::upickle::3.3.1
-//> using dep com.raquo::laminar-shoelace::0.1.0
+//> using dep com.raquo::laminar::17.1.0
 
 //> using jsModuleKind es
 //> using jsModuleSplitStyleStr smallmodulesfor
@@ -18,15 +15,7 @@ package webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
-import com.raquo.laminar.api.L
 import com.raquo.laminar.api.L.{*, given}
-import com.raquo.waypoint._
-import upickle.default._
-import com.raquo.laminar.shoelace.sl.Badge
-import com.raquo.laminar.shoelace.sl.CommonKeys.variant
-import com.raquo.laminar.shoelace.sl.Include
-import com.raquo.laminar.shoelace.sl.Menu
-import com.raquo.laminar.shoelace.sl.MenuItem
 
 @main
 def main: Unit =
--- justfile
+++ justfile
@@ -2,4 +2,4 @@ setup-ide:
   scala-cli setup-ide .
 
 dev:
-  cs launch --contrib sjsls -- --port 3001 --client-routes-prefix app --styles-dir {{invocation_directory()}}/styles
+  cs launch --contrib sjsls -- --styles-dir {{invocation_directory()}}/styles
