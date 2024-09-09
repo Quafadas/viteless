85b1f4c62a0e46026562c223c8a3459a527d560a
Simon Parten <quafadas@gmail.com>
2024-09-09 15:59:44 +0200

    .

--- hello.scala
+++ hello.scala
@@ -8,12 +8,20 @@
 //> using jsModuleSplitStyleStr smallmodulesfor
 //> using jsSmallModuleForPackage webapp
 //> using jsEmitSourceMaps true
+//> using jsEsModuleImportMap importmap.json
+
+//> using dep io.github.quafadas::peer-scalajs::0.0.3
+
 
 package webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
 import com.raquo.laminar.api.L.{*, given}
+import io.github.quafadas.peerscalajs.Peer
+import io.github.quafadas.peerscalajs.DataConnection
+
+import scala.scalajs.js
 
 @main
 def main: Unit =
@@ -23,20 +31,84 @@ def main: Unit =
   )
 
 def app =
-  val hiVar = Var("Scala JS") // Local state
+  val hiVar = Var("Simon")
+  val connectToVar = Var("simon2")
+  val sendMessageVar = Var("")
+  val dataConnection = Var[Option[DataConnection]](None)
+
+  val dataConnectionSignal = dataConnection.signal.map(_.map(_.on("data", (data: js.Any) => {
+    println(s"Received data: $data")
+  })))
+
+  val peer = hiVar.signal.map { id =>
+    val p = new Peer(id = id)
+    p.on("connection", (data: DataConnection) => {
+
+        data.on("data", (data: js.Object) => {
+          println(s"Received data: ${js.JSON.stringify(data)}")
+        })
+        dataConnection.set(Some(data))
+        println(s"Received data connection from ${data.peer}")
+    })
+    p
+  }
+
+  val connectionPair = peer.combineWith(connectToVar.signal)
+
   div(
     h1(
-      s"Hello ",
+      s"",
       child.text <-- hiVar.signal
     ),
-    p("This page should reload on change. Check the justfile... for the command to run the server."),
+    p(
+      child.text <-- peer.map{p => s"Your peer id is ${p.id}"}
+    ),
     // https://demo.laminar.dev/app/form/controlled-inputs
-    input(
+    p(input(
       typ := "text",
       controlled(
         value <-- hiVar.signal,
         onInput.mapToValue --> hiVar.writer
       )
+    )),
+    p(
+      s"Connect to peer with id"
     ),
-    more
+    p(input(
+      typ := "text",
+      controlled(
+        value <-- connectToVar.signal,
+        onInput.mapToValue --> connectToVar.writer
+      )
+    ),
+      "establish connection",
+      connectionPair--> Observer[(Peer, String)]{
+        case (p, inId) =>
+          val data = p.connect(inId)
+          data.on("data", (data: js.Object) => {
+            println(s"Received data: ${js.JSON.stringify(data)}")
+          })
+          dataConnection.set(Some(data))
+      }
+    ),
+    p(
+      "send this message to the connected peer",
+      input(
+      typ := "text",
+      controlled(
+        value <-- sendMessageVar.signal,
+        onInput.mapToValue --> sendMessageVar.writer
+      )
+    )),
+
+    dataConnection.signal.combineWith(sendMessageVar.signal)  -->
+    Observer[(Option[DataConnection], String)]{
+        case (dataConn, msg) =>
+          println(s"sending message: $msg")
+          dataConn.foreach(_.send(msg))
+    }
+
+
+
+
   )
